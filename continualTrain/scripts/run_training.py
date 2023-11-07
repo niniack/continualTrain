f8a8054223471b29474d3e1bb8f7d0c57851f1a2
niniack <nsa325@nyu.edu>
2023-11-07 13:47:16 +0400

    log nh_weight

--- continualTrain/scripts/run_training.py
+++ continualTrain/scripts/run_training.py
@@ -210,7 +210,9 @@ def main():
                 "model_class": type(model).__name__,
                 "optimizer_type": type(optimizer).__name__,
                 "experiences": int(dataset.n_experiences),
+                "nh_weight": metadata["nh_weight"] if "nh_weight" in metadata else 0,
             }
+
             for key, value in optimizer.defaults.items():
                 wandb_config_dict[f"optimizer_{key}"] = value
 
