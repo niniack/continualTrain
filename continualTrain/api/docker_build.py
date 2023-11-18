545fe8beeeddcd551bdae95473c6ea829ae60f0e
niniack <nsa325@nyu.edu>
2023-11-19 02:00:49 +0400

    Split dependencies for faster build

--- continualTrain/api/docker_build.py
+++ continualTrain/api/docker_build.py
@@ -6,23 +6,36 @@ from rich import print
 from continualTrain.api.utils import get_latest_commit_sha
 
 
-def build_docker_image(add_deps: str, image_name: str, push: bool) -> None:
-    dependencies_str = " ".join(add_deps)
+def build_docker_image(add_deps: list, image_name: str, push: bool) -> None:
+    non_git_deps = []
+    git_deps = []
+    for dep in add_deps:
+        if dep.startswith("git+"):
+            git_deps.append(dep)
+        else:
+            non_git_deps.append(dep)
+
+    non_git_deps_str = " ".join(non_git_deps)
+    git_deps_str = " ".join(git_deps)
+
     script_dir = Path(__file__).resolve().parent
     parent_dir = script_dir.parent
     docker_dir = parent_dir / "docker"
     LATEST_COMMIT = get_latest_commit_sha(
         "https://github.com/niniack/continualUtils", "dev"
     )
+
     command = [
         "docker",
         "build",
         "-t",
         image_name,
         "--build-arg",
-        f"CACHEBUSTER=${LATEST_COMMIT}",
+        f"CACHEBUSTER={LATEST_COMMIT}",
         "--build-arg",
-        f"ADDITIONAL_DEPS={dependencies_str}",
+        f"ADDITIONAL_DEPS={non_git_deps_str}",
+        "--build-arg",
+        f"GIT_DEPS={git_deps_str}",
         "-f",
         str(docker_dir / "Dockerfile"),
         str(parent_dir.parent),
