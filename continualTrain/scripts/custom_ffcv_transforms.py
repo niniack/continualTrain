dbe6a4b4ff7fb94347b1d4bc66e7b6e8780092c2
niniack <nsa325@nyu.edu>
2023-12-14 13:50:21 +0400

    deterministic flips

--- continualTrain/scripts/custom_ffcv_transforms.py
+++ continualTrain/scripts/custom_ffcv_transforms.py
@@ -29,8 +29,17 @@ class RandomHorizontalFlipSeeded(Operation):
         flip_prob = self.flip_prob
 
         def flip(images, dst, indices):
-            np.random.seed(indices[-1])
-            should_flip = rand(images.shape[0]) < flip_prob
+            # np.random.seed(12345)
+
+            ###
+            # Maybe a bad idea
+            # But this is deterministic
+            num_images = images.shape[0]
+            num_flips = int(num_images * flip_prob)
+            should_flip = np.zeros(num_images, dtype=np.bool_)
+            should_flip[:num_flips] = True
+            ###
+
             for i in my_range(images.shape[0]):
                 if should_flip[i]:
                     dst[i] = images[i, :, ::-1]
